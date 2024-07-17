d9f99ebad77f8c3785a8b3375c1b25008ec337e8
Sun Jiajun <1580736133@qq.com>
2024-07-17 09:27:57 +0800

    Update ArrayDeque.java

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -4,51 +4,59 @@ import java.util.Iterator;
 
 public class ArrayDeque<T> implements deque<T>{
     private int size;
-    private int pSize;
-    private int mSize;
-    private int capacity;
-    private int mid;
+    private int First;
+    private int Last;
     private T[] items;
+    private int capacity;
     public ArrayDeque() {
         size = 0;
-        pSize = 0;
-        mSize = -1;
+        items = (T[]) new Object[8];
         capacity = 8;
-        mid = 4;
-        items = (T[]) new Object[capacity];
+        First = -1;
+        Last = 0;
     }
     private void resizing(int Capacity) {
-        T[] a = (T[]) new Object[Capacity];
-        System.arraycopy(items, 0, a, 0, size);
-        items = a;
+        T[] newItems = (T[]) new Object[Capacity];
+        if (First >= Last) {
+            System.arraycopy(items, First+1, newItems, 0, size - Last);
+            System.arraycopy(items, 0, newItems, size - Last, Last + 1);
+        } else {
+            System.arraycopy(items, First+1, newItems, 0, size);
+        }
+        First = Capacity - 1;
+        Last = size;
+
+//        System.arraycopy(items, 0, newItems, 0, size);
+        items = newItems;
         capacity = Capacity;
-        mid = capacity / 2;
     }
+
     private void Resizing() {
         double R = size * 1.0 / capacity;
         while (R < 0.25) {
             capacity /= 2;
             R = size * 1.0 / capacity;
         }
-        mid = capacity / 2;
         resizing(capacity);
+    }
+    public void addCheck() {
+        if (size == capacity - 1) {
+            resizing(capacity * 2);
+        }
     }
     @Override
     public void addFirst(T item) {
-        items[mid + mSize--] = item;
+        if (First < 0)  First = capacity - 1;
+        items[First--] = item;
         size++;
-        if(size == capacity - 1){
-            resizing(size * 2);
-        }
+        addCheck();
     }
 
     @Override
     public void addLast(T item) {
-        items[mid + pSize++] = item;
+        items[Last++] = item;
         size++;
-        if(size == capacity - 1){
-            resizing(size * 2);
-        }
+        addCheck();
     }
 
     @Override
@@ -63,7 +71,10 @@ public class ArrayDeque<T> implements deque<T>{
 
     @Override
     public void printDeque() {
-        for (int i = mid + mSize + 1; i < mid + pSize; i ++) {
+        for (int i = First; i < capacity; i++){
+            System.out.println(items[i]);
+        }
+        for (int i = 0; i <= Last; i++){
             System.out.println(items[i]);
         }
     }
@@ -71,26 +82,35 @@ public class ArrayDeque<T> implements deque<T>{
     @Override
     public T removeFirst() {
         if (size == 0)  return null;
-        T First = items[mid + mSize + 1];
-        mSize++;
+        T firstItem;
+        if (First < capacity - 1) {
+            First++;
+        } else {
+            First = 0;
+        }
+        firstItem = get(First);
         size--;
-        Resizing();
-        return First;
+        if (size != 0) Resizing();
+        return firstItem;
     }
 
     @Override
     public T removeLast() {
         if (size == 0)  return null;
-        T Last = items[mid + pSize - 1];
-        pSize--;
+        if (Last > 0) {
+            Last--;
+        } else {
+            Last = capacity - 1;
+        }
+        T lastItem = get(Last);
         size--;
         Resizing();
-        return Last;
+        return lastItem;
     }
 
     @Override
     public T get(int index) {
-        return items[mid + index + mSize + 1];
+        return items[index];
     }
 
     @Override
@@ -107,7 +127,6 @@ public class ArrayDeque<T> implements deque<T>{
         public boolean hasNext() {
             return pos < size;
         }
-
 
         public T next() {
             return items[pos++];
