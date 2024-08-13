ded810c38af244ca01375489be6bbd8d0f000078
wrench1024 <1580736133@qq.com>
2024-08-13 19:23:42 +0800

    8.13 19:23

---
+++ lab8/hashmap/MyHashMap.java
@@ -0,0 +1,289 @@
+package hashmap;
+
+import java.util.*;
+
+/**
+ *  A hash table-backed Map implementation. Provides amortized constant time
+ *  access to elements via get(), remove(), and put() in the best case.
+ *
+ *  Assumes null keys will never be inserted, and does not resize down upon remove().
+ *  @author YOUR NAME HERE
+ */
+public class MyHashMap<K, V> implements Map61B<K, V> {
+    int capacity = 16;
+    int size = 0;
+    double loadFactor = 0.25;
+
+    /**
+     * Removes all the mappings from this map.
+     */
+    @Override
+    public void clear() {
+        capacity = 16;
+        size = 0;
+        buckets = createTable(capacity);
+    }
+
+    /**
+     * Returns true if this map contains a mapping for the specified key.
+     *
+     * @param key
+     */
+    @Override
+    public boolean containsKey(K key) {
+        return get(key) != null;
+    }
+
+    /**
+     * Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
+     *
+     * @param key
+     */
+    @Override
+    public V get(K key) {
+        V value = null;
+        int index = (key.hashCode() & 0x7fffffff) % buckets.length;
+        Collection<Node> thisBucket = buckets[index];
+        if (thisBucket == null) {
+            return null;
+        }
+        for (Node thisNode : thisBucket) {
+            if (thisNode.key.equals(key)) {
+                value = thisNode.value;
+            }
+        }
+        return value;
+    }
+
+    /**
+     * Returns the number of key-value mappings in this map.
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    public Collection<Node>[] setCapacity (int newCapacity) {
+        Collection<Node>[] newBuckets = createTable(newCapacity);
+        for (Collection<Node> bucket : buckets) {
+            if (bucket != null) {
+                for (Node node : bucket) {
+                    put(node.key, node.value, newBuckets);
+                }
+            }
+        }
+        return newBuckets;
+    }
+
+    public void resize() {
+        double currentFactor = 1.0 * size / capacity;
+        if (currentFactor >= loadFactor) {
+            capacity *= 2;
+            buckets = setCapacity(capacity);
+        }
+    }
+
+    /**
+     * Associates the specified value with the specified key in this map.
+     * If the map previously contained a mapping for the key,
+     * the old value is replaced.
+     *
+     * @param key
+     * @param value
+     */
+    @Override
+    public void put(K key, V value) {
+        put(key, value, buckets);
+    }
+
+    public void put(K key, V value, Collection<Node>[] thisBuckets) {
+        int index = ((key.hashCode() & 0x7fffffff) % capacity);
+        if (thisBuckets[index] == null) {
+            thisBuckets[index] = createBucket();
+        }
+        if (!containsKey(key)) {
+            size++;
+        }
+        Node newNode = createNode(key, value);
+        thisBuckets[index].add(newNode);
+        resize();
+    }
+
+    /**
+     * Returns a Set view of the keys contained in this map.
+     */
+    @Override
+    public Set<K> keySet() {
+        HashSet thisSet = new HashSet<>();
+        for (Collection<Node> thisBucket : buckets) {
+            if (thisBucket != null) {
+                for (Node thisNode : thisBucket) {
+                    thisSet.add(thisNode.key);
+                }
+            }
+        }
+        return thisSet;
+    }
+
+    /**
+     * Removes the mapping for the specified key from this map if present.
+     * Not required for Lab 8. If you don't implement this, throw an
+     * UnsupportedOperationException.
+     *
+     * @param key
+     */
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException("Operation not supported");
+
+    }
+
+    /**
+     * Removes the entry for the specified key only if it is currently mapped to
+     * the specified value. Not required for Lab 8. If you don't implement this,
+     * throw an UnsupportedOperationException.
+     *
+     * @param key
+     * @param value
+     */
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException("Operation not supported");
+    }
+
+    private class HashMapIterator implements Iterator<K> {
+        private int pos;
+        HashMapIterator() {
+            pos = 0;
+        }
+        /**
+         * Returns {@code true} if the iteration has more elements.
+         * (In other words, returns {@code true} if {@link #next} would
+         * return an element rather than throwing an exception.)
+         *
+         * @return {@code true} if the iteration has more elements
+         */
+        @Override
+        public boolean hasNext() {
+            return pos < size;
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if the iteration has no more elements
+         */
+        @Override
+        public K next() {
+            return null;
+        }
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<K> iterator() {
+        return new HashMapIterator();
+    }
+
+    @Override
+    public int hashCode() {
+        int hashCode = 1;
+        for (Object o : this) {
+            hashCode = hashCode * 31 + o.hashCode();
+        }
+        return hashCode % capacity;
+    }
+
+    /**
+     * Protected helper class to store key/value pairs
+     * The protected qualifier allows subclass access
+     */
+    protected class Node {
+        K key;
+        V value;
+
+        Node(K k, V v) {
+            key = k;
+            value = v;
+        }
+    }
+
+    /* Instance Variables */
+    private Collection<Node>[] buckets;
+    // You should probably define some more!
+
+    /** Constructors */
+    public MyHashMap() {
+        buckets = createTable(capacity);
+    }
+
+    public MyHashMap(int initialSize) {
+        capacity = initialSize;
+        buckets = createTable(capacity);
+    }
+
+    /**
+     * MyHashMap constructor that creates a backing array of initialSize.
+     * The load factor (# items / # buckets) should always be <= loadFactor
+     *
+     * @param initialSize initial size of backing array
+     * @param maxLoad maximum load factor
+     */
+    public MyHashMap(int initialSize, double maxLoad) {
+        capacity = initialSize;
+        loadFactor = maxLoad;
+        buckets = createTable(capacity);
+    }
+
+    /**
+     * Returns a new node to be placed in a hash table bucket
+     */
+    private Node createNode(K key, V value) {
+        return new Node(key, value);
+    }
+
+    /**
+     * Returns a data structure to be a hash table bucket
+     *
+     * The only requirements of a hash table bucket are that we can:
+     *  1. Insert items (`add` method)
+     *  2. Remove items (`remove` method)
+     *  3. Iterate through items (`iterator` method)
+     *
+     * Each of these methods is supported by java.util.Collection,
+     * Most data structures in Java inherit from Collection, so we
+     * can use almost any data structure as our buckets.
+     *
+     * Override this method to use different data structures as
+     * the underlying bucket type
+     *
+     * BE SURE TO CALL THIS FACTORY METHOD INSTEAD OF CREATING YOUR
+     * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
+     */
+    protected Collection<Node> createBucket() {
+        return new LinkedList<>();
+    }
+
+    /**
+     * Returns a table to back our hash table. As per the comment
+     * above, this table can be an array of Collection objects
+     *
+     * BE SURE TO CALL THIS FACTORY METHOD WHEN CREATING A TABLE SO
+     * THAT ALL BUCKET TYPES ARE OF JAVA.UTIL.COLLECTION
+     *
+     * @param tableSize the size of the table to create
+     */
+    private Collection<Node>[] createTable(int tableSize) {
+        return new Collection[tableSize];
+    }
+
+    // TODO: Implement the methods of the Map61B Interface below
+    // Your code won't compile until you do so!
+
+}
